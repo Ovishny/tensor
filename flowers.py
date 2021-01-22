2e86bf04ab16a77ac55b04ee53faacc90f1bed98
Oren Vishny <ovishny@gmail.com>
2021-01-22 18:33:15 -0500

    Update flowers.py

--- flowers.py
+++ flowers.py
@@ -1,4 +1,4 @@
-#Using DNN(Deep Neural Networks) to predict the species of flower based on its characteristics
+#Using DNN(Deep Neural Networks) classifier to predict the species of flower based on its characteristics
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -18,3 +18,26 @@ test = pd.read_csv(test_path, names = CSV_COLUMN_NAMES, header = 0)
 #pop out the column we are trying to predict
 train_y = train.pop('Species')
 test_y = test.pop('Species')
+#defining the input function
+def input_fn(features, labels, training = True, batch_size = 256):
+	#convert input to dataset
+	dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+	#shuffle and repeat if training
+	if training:
+		dataset = dataset.shuffle(1000).repeat()
+
+	return dataset.batch(batch_size)
+
+#create feature columns
+my_feature_columns = []
+for key in train.keys():
+	my_feature_columns.append(tf.feature_column.numeric_column(key = key))
+
+#build DNN with 2 hidden layers with 30 and 10 hidden nodes each
+
+classifier = tf.estimator.DNNClassifier(feature_columns = my_feature_columns,
+	#two hidden layers of 30 and 10 nodes
+	hidden_units = [30,10],
+	#model must choose between three classes
+	n_classes = 3)
