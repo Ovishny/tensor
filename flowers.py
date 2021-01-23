4400fb3383781c571137c9cbc71b97030fd8747f
Oren Vishny <ovishny@gmail.com>
2021-01-22 19:01:19 -0500

    Update flowers.py

--- flowers.py
+++ flowers.py
@@ -47,5 +47,30 @@ classifier.train(
 	input_fn = lambda: input_fn(train, train_y, training = True),
 	steps = 5000)
 
+#evaluates accuracy of model
 eval_result = classifier.evaluate(input_fn = lambda: input_fn(test, test_y, training = False))
 print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+#this will allow a user to input info --------------------------------------------
+def input_fn(features, batch_size = 256):
+	#convert input to dataset without labels
+	return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print("Please type numeric values as prompted.")
+for feature in features:
+	valid = True
+	while valid:
+		val = input(feature + ": ")
+		if not val.isdigit(): valid = False
+	predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn = lambda: input_fn(predict))
+for pred_dict in predictions:
+	class_id = pred_dict['class_ids'][0]
+	probability = pred_dict['probabilities'][class_id]
+
+	print('Prediction is "{}" ({:.1f}%)'.format(
+		SPECIES[class_id], 100*probability))
