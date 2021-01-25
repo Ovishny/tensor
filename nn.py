8706b6183833c1e5d89c67579af3ceab7a1f1593
Oren Vishny <ovishny@gmail.com>
2021-01-25 18:44:27 -0500

    Update nn.py

--- nn.py
+++ nn.py
@@ -3,3 +3,8 @@ from tensorflow import keras
 
 import numpy as np
 import matplotlib.pyplot as plt 
+
+#load in dataset
+fashion_mnist = keras.datasets.fashion_mnist 
+#split data into testing and training
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
