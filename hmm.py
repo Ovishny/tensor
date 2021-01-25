b29ca166ab8ce044790ed4f6d9c105c5e079f542
Oren Vishny <ovishny@gmail.com>
2021-01-24 21:04:47 -0500

    Update hmm.py

--- hmm.py
+++ hmm.py
@@ -1,2 +1,7 @@
 import tensorflow_probability as tfp 
 import tensorflow as tf
+
+tfd = tfp.distributions
+initial_distribution = tfd.Categorical(probs = [0.8, 0.2]) #80% chance cold, 20% chance hot
+transitional_distribution = tfd.Categorical(probs = [[0.7,0.3], [0.2, 0.8]])#70% going from cold to cold, #20% from hot to cold
+observation_distribution = tfd.Normal(loc = [0., 15.], scale = [5., 10.])#loc is mean and scale is standard deviation for cold and hot respective
