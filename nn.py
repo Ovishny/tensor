d2f48dfcb8f5ca41482ea113fd95a91e6afec065
Oren Vishny <ovishny@gmail.com>
2021-01-25 19:32:51 -0500

    Update nn.py

--- nn.py
+++ nn.py
@@ -9,6 +9,10 @@ fashion_mnist = keras.datasets.fashion_mnist
 #split data into testing and training
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+#class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 #preprocess to fit pixel values between 0 and 1
 train_images = train_images/255.0
 test_images = test_images/255.0
