0b620919d825a10f0bda5173f217c6d83090cb47
Oren Vishny <ovishny@gmail.com>
2021-01-29 17:57:58 -0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -46,3 +46,5 @@ history = model.fit(train_images, train_labels, epochs = 10,
 	validation_data = (test_images, test_labels))
 
 #Evaluating the model
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 2)
+print(test_acc)
