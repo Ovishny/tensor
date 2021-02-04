f695a5d1de2e617118baabd1120b8037514a04ab
Oren Vishny <ovishny@gmail.com>
2021-02-03 19:50:03 -0500

    Update rnn_movie.py

--- rnn_movie.py
+++ rnn_movie.py
@@ -11,4 +11,23 @@ VOCAB_SIZE = 88584
 MAXLEN = 250
 BATCH_SIZE = 64
 
+#split data from dataset
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = VOCAB_SIZE)
+
+#padding reviews so each review has same length when passed in
+train_data = sequence.pad_sequences(train_data, MAXLEN)
+test_data = sequence.pad_sequences(test_data, MAXLEN)
+
+#creating the model
+model = tf.keras.Sequential([
+	tf.keras.layers.Embedding(VOCAB_SIZE,32), #word embedding layer
+	tf.keras.layers.LSTM(32), #LSTM layers
+	tf.keras.layers.Dense(1, activation = 'sigmoid') #Dense layer. Sigmoid squishes values between 0 and 1. <.5 is negative and >.5 is positive
+])
+
+#train the model
+model.compile(loss = "binary_crossentropy",
+	optimize = 'rmsprop',
+	metrics = ['acc']
+)
+history = model.fit(train_Data, train_labels, epochs = 10, validation_split = 0.2)
