c520347e833ea65f5968c506f678fa7997bcd4b6
Oren Vishny <ovishny@gmail.com>
2021-02-03 19:33:06 -0500

    Update rnn_movie.py

--- rnn_movie.py
+++ rnn_movie.py
@@ -1,1 +1,14 @@
-#recurrent neural network
+#Natural Language Processing on Movie Revies using recurrent neural network
+
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+import tensorflow as tf 
+import os 
+import numpy as np 
+
+VOCAB_SIZE = 88584
+
+MAXLEN = 250
+BATCH_SIZE = 64
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = VOCAB_SIZE)
