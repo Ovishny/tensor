7a0052c7156f4dcd3d4570d060117a225e403bdd
Oren Vishny <ovishny@gmail.com>
2021-02-13 21:41:52 -0500

    Update captions.py

--- captions.py
+++ captions.py
@@ -29,3 +29,15 @@ if not os.path.exists(os.path.abspath('.') + annotation_folder):
 		extract = True)
 	annotation_file = os.path.dirname(annotation_zip) + '/annotations/captions_train2014.json'
 	os.remove(annotation_zip)
+
+#download image files
+image_folder = '/train2014/'
+if not os.path.exists(os.path.abspath('.') + image_folder):
+	image_zip = tf.keras.utils.get_file('train2014.zip',
+		cache_subdir = os.path.abspath('.'),
+		origin = 'http://images.cocodataset.org/zips/train2014.zip',
+		extract = True)
+	PATH = os.path.dirname(image_zip) + image_folder
+	os.remove(image_zip)
+else:
+	PATH = os.path.abspath('.') + image_folder
