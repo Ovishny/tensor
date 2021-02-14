f8b7e8783fb2fba6d0ed52b6e3c8d472f5c67c5f
Oren Vishny <ovishny@gmail.com>
2021-02-13 21:36:38 -0500

    Update captions.py

--- captions.py
+++ captions.py
@@ -19,3 +19,13 @@ import json
 from glob import glob
 from PIL import Image 
 import pickle
+
+#download caption annotation files
+annotation_folder = '/annotations/'
+if not os.path.exists(os.path.abspath('.') + annotation_folder):
+	annotation_zip = tf.keras.utils.get_file('captions.zip',
+		cache_subdir = os.path.abspath('.'),
+		origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+		extract = True)
+	annotation_file = os.path.dirname(annotation_zip) + '/annotations/captions_train2014.json'
+	os.remove(annotation_zip)
