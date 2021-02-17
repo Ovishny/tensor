2d7c2573a2cc47446aaae668d256751c97e17f73
Oren Vishny <ovishny@gmail.com>
2021-02-16 19:38:30 -0500

    Update captions.py

--- captions.py
+++ captions.py
@@ -254,3 +254,28 @@ class RNN_Decoder(tf.keras.Model):
 		self.fc2 = tf.keras.layers.Dense(vocab_size)
 
 		self.attention = BahdanauAttention(self.units)
+
+	def call(self, x, features, hidden):
+		#define attention as model
+		context_vector, attention_weights = self.attention(features, hidden)
+
+		#x shape after passing through embedding == batch size, 1, embedding_Dim
+		x = self.embedding(x)
+
+		#x shape after concatenation == batch_size, 1, embedding_dim + hidden_size
+		x = tf.concat([tf.expand_dims(context_vector, 1), x], axis = -1)
+
+		#pass concatenated vector to gru
+		output, state = self.gru(x)
+
+		#shape == batch_size, max_length, hidden_size
+		x = self.fc1(output)
+
+		#x shape == batch_size*max_length, hidden_size
+		x = tf.reshape(x, (-1,x.shape[2]))
+
+		#output shape == batch_size*max_length, vocab
+		x = self.fc2(x)
+
+		return x, state, attention_weights
+		
