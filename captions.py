574a66ee74ab9b4a64a992a92766dd925eadd237
Oren Vishny <ovishny@gmail.com>
2021-02-17 22:45:01 -0500

    Update captions.py

--- captions.py
+++ captions.py
@@ -324,3 +324,27 @@ loss_plot = []
 
 @tf.function
 def train_step(img_tensor,target):
+	loss = 0
+
+	#initialize hidden state for each batch
+	#captions not related from image to image
+	hidden = decoder.reset_state(batch_size = target.shape[0])
+
+	dec_input = tf.expand_dims([tokenizer.word_index['<start>']]*target.shape[0], 1)
+	with tf.GradientTape() as tape:
+		features = encoder(img_tensor)
+
+		for i in range(i, target.shape[1]):
+			#pass features through decoder
+			predictions, hidden, _ = decoder(dec_input, features, hidden)
+
+			loss += loss_function(target[:, i], predictions)
+
+			#using teacher forcing
+			dec_input = tf.expand_dims(target[:, i], 1)
+
+	total_loss = (loss/int(target.shape[1]))
+	training_variables = encoder.trainable_variables + decoder.trainable_variables
+	gradients = tape.gradient(loss, trainable_variables)
+	optimizer.apply_gradients(zip(gradients, trainable_variables))
+	return loss, total_loss
