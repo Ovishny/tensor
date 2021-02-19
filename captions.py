439bdb4fbf9b64b44479ad093e834ae7c13386c0
Oren Vishny <ovishny@gmail.com>
2021-02-18 20:36:42 -0500

    Update captions.py

--- captions.py
+++ captions.py
@@ -412,3 +412,24 @@ def evaluate(image):
 
 	attention_plot = attention_plot[:len(result), :]
 	return result, attention_plot
+
+def plot_attention(image, result, attention_plot):
+	temp_image = np.array(Image.open(image))
+
+	fig = plt.figure(figsize = (10, 10))
+
+	len_result = len(result)
+	for l in range(len_result):
+		temp_att = np.resize(attention_plot[l], (8,8))
+		ax = fig.add_subplot(len_result//2, len_result//2, l+1)
+		ax.set_title(result[l])
+		img = ax.imshow(temp_image)
+		ax.imshow(temp_att, cmap ='gray', alpha = 0.6, extent = img.get_extent())
+
+	plt.tight_layout()
+	plt.show()
+
+#captions on validation set
+rid = np.random.randint(0, len(img_name_val))
+image = img_name_val[rid]
+real_caption = ' '.join([tokenizer.index_word])
