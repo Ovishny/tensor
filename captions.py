c0d7452fbdc6059acc14317dff36fa3f7090a178
Oren Vishny <ovishny@gmail.com>
2021-02-18 20:30:28 -0500

    Update captions.py

--- captions.py
+++ captions.py
@@ -376,3 +376,39 @@ for epoch in range(start_epoch, EPOCHS):
 # plt.ylabel('Loss')
 # plt.title('Loss Plot')
 # plt.show()
+
+#Caption
+#evaluate function similar to training loop, dont use teacher forcing.
+#input to decoder at each step is previous predicions along with hidden state and encoder output
+#stop predicting when model predicts end token
+#store attention weights for every step
+
+def evaluate(image):
+	attention_plot = np.zeros((max_length, attention_features_shape))
+
+	hidden = decoder.reset_state(batch_size = 1)
+
+	temp_input = tf.expand_dim(load_image(image)[0], 0)
+	img_tensor_val = image_features_extract_model(temp_input)
+	img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+	features = encoder(img_tensor_val)
+
+	dec_input = tf.expand_dims([tokenizer.word_index['<start>']],0)
+	result = []
+
+	for i in range(max_length):
+		predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+		attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+
+		predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+		result.append(tokenizer.index_word[predicted_id])
+
+		if tokenizer.index_word[predicted_id] == '<end>':
+			return result, attention_plot
+
+		dec_input = tf.expand_dims([predicted_id], 0)
+
+	attention_plot = attention_plot[:len(result), :]
+	return result, attention_plot
